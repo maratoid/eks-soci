eeec3fc60a8eb19386e24a39d48de3e9a7f666f5
Marat Garafutdinov <maratoid@gmail.com>
2026-04-14 14:35:43 -0700

    kubeconfig conveniences

--- terraform/kubeconfig.tf
+++ terraform/kubeconfig.tf
@@ -3,23 +3,14 @@ resource "multipass_file_download" "kubeconfig" {
   source      = "/etc/rancher/k3s/k3s.yaml"
   destination = abspath("${path.module}/../kubeconfig.multipass")
 
-  lifecycle {
-    action_trigger {
-      events  = [after_create]
-      actions = [action.local_command.merge_kubeconfigs]
-    }
-  }
-}
-
-action "local_command" "merge_kubeconfigs" {
-  config {
-    command = "sh"
-    arguments = [
+  provisioner "local-exec" {
+    interpreter = ["sh"]
+    command = join("", [
       "${path.module}/scripts/merge-kubeconfigs.sh",
       abspath("${path.module}/../kubeconfig.multipass"),
       multipass_instance.master_node.ipv4,
       "${var.merge_kubeconfigs}",
       var.merged_cluster_name
-    ]
+    ])
   }
 }
